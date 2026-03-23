1006cc83b1f1dc47f202913508c3d58e75eeb836
JaumePuig <jaumepuigsoriano@gmail.com>
2026-03-23 10:39:34 +0100

    ejercicio alumno a

---
+++ assets/js/alumno_a.js
@@ -0,0 +1,15 @@
+// TRABAJO DEL ALUMNO A
+console.log("--- Gestión de Alumno A ---");
+
+let flota = ["Madrid", "Valencia", "Sevilla"];
+// Tu código aquí:
+console.log(flota);
+console.log("Nuevo camion");
+flota.push("Barcelona");
+console.log(flota);
+console.log("Camion de urgencia");
+flota.unshift("Malaga");
+console.log(flota);
+console.log("Camion en ruta");
+flota.pop();
+console.log(flota);
